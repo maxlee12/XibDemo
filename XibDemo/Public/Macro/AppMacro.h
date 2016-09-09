a17262656da38992e42d7f0b7c0c8b78648c2ea4
lawrence <law@xqopen.com>
2016-09-09 16:25:40 +0800

    架构

---
+++ XibDemo/Public/Macro/AppMacro.h
@@ -0,0 +1,13 @@
+//
+//  AppMacro.h
+//  XibDemo
+//
+//  Created by lawrence on 16/9/9.
+//  Copyright © 2016年 lawrence. All rights reserved.
+//
+
+#ifndef AppMacro_h
+#define AppMacro_h
+
+
+#endif /* AppMacro_h */
---
+++ XibDemo/Public/Macro/VendorMacro.h
@@ -0,0 +1,13 @@
+//
+//  VendorMacro.h
+//  XibDemo
+//
+//  Created by lawrence on 16/9/9.
+//  Copyright © 2016年 lawrence. All rights reserved.
+//
+
+#ifndef VendorMacro_h
+#define VendorMacro_h
+
+
+#endif /* VendorMacro_h */
